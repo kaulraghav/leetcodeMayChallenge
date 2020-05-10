0a425609f02950036facfed0159d8654b12553ae
RaghavKaul <kaulraghav@gmail.com>
2020-05-09 20:24:41 -0700

    Update (09.05.2020) jewelsAndStones.py

--- (09.05.2020) jewelsAndStones.py
+++ (09.05.2020) jewelsAndStones.py
@@ -8,3 +8,14 @@ class Solution:
                 if (i == j):
                     jewels += 1
         return jewels
+    
+    # Using sets to only compare to unique J's to S, also if i in s loop [Time - 28ms (> 76.05%), Memory - 14MB]
+
+class Solution:
+    def numJewelsInStones(self, J: str, S: str) -> int:
+        jset = set(J)
+        jewels = 0
+        for i in S:
+            if i in jset:
+                jewels += 1
+        return jewels
